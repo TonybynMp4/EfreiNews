12d56820cbaa785e2318f78c22b96b23bfe30bf0
Tony <97451137+TonybynMp4@users.noreply.github.com>
2024-10-13 23:56:52 +0200

    feat: db schema

---
+++ db.sql
@@ -0,0 +1,21 @@
+CREATE DATABASE IF NOT EXISTS `efreinews`;
+
+USE `efreinews`;
+
+CREATE TABLE IF NOT EXISTS `users` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `username` varchar(255) NOT NULL,
+    `password` varchar(255) NOT NULL,
+    `role` ENUM('admin', 'editor', 'user') NOT NULL DEFAULT 'user',
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8;
+
+CREATE TABLE IF NOT EXISTS `articles` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `author` int(11) NOT NULL,
+    `title` varchar(255) NOT NULL,
+    `content` text NOT NULL,
+    `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`author`) REFERENCES `users`(`id`)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8;
