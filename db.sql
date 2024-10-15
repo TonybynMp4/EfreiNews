a7c5318be863afa85310df45e5e560714cfe72f0
Tony <97451137+TonybynMp4@users.noreply.github.com>
2024-10-16 00:35:40 +0200

    feat: remove unnecessary role

--- db.sql
+++ db.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS `users` (
     `id` int(11) NOT NULL AUTO_INCREMENT,
     `username` varchar(255) NOT NULL,
     `password` varchar(255) NOT NULL,
-    `role` ENUM('admin', 'editor', 'user') NOT NULL DEFAULT 'user',
+    `role` ENUM('admin', 'user') NOT NULL DEFAULT 'user',
     PRIMARY KEY (`id`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8;
 
