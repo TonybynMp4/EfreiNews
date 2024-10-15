b07dfee52f210b14a1092c36baf4e40f03b1b648
Tony <97451137+TonybynMp4@users.noreply.github.com>
2024-10-16 00:40:36 +0200

    fix: fix condition & links

--- App/views/header.php
+++ App/views/header.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Efrei News</title>
+    <link rel="stylesheet" href="/efreinews/public/css/style.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Google+Sans:400,500,600,700|Product+Sans:400&display=swap&lang=en">
     <script src="https://cdn.jsdelivr.net/npm/marked/marked.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@alpinejs/persist@latest/dist/cdn.min.js"></script>
@@ -23,10 +24,10 @@
 </head>
 
 <body x-data x-bind:class="$store.darkMode.on ? 'dark' : ''">
-<header style="border-bottom: #1F3A93 5px solid;">
-    <a href="home" style="display: flex; align-items: center; gap: 1em; text-decoration: none; color: var(--text-color);">
-        <img src="/Mvc/public/images/logo.webp" alt="Logo Efrei News" style="width: 4em; height: 4em;">
-        <h1>
+<header style="border-bottom: #1F3A93 5px solid; padding-block: 1em;">
+    <a href="/efreinews/public/Home/index" style="display: flex; align-items: center; gap: 1em; text-decoration: none; color: var(--text-color);">
+        <img src="/efreinews/public/images/logo.webp" alt="Logo Efrei News" style="width: 3.5em; height: 3.5em;">
+        <h1 style="margin: 0;">
             Efrei News
         </h1>
     </a>
@@ -34,14 +35,14 @@
     <div>
         <button x-cloak @click="$store.darkMode.toggle()" x-text="$store.darkMode.on ? '☀️' : '🌙'"></button>
         <nav>
-            <a class="button" href="/Mvc/public/Home/index">Home</a>
+            <a class="button" href="/efreinews/public/Home/index">Home</a>
             <?php if (isset($_SESSION['user'])): ?>
-                <?php if ($_SESSION['user']['role'] === 'admin'): ?>
-                    <a class="button" href="/Mvc/public/Admin/index">Admin</a>
+                <?php if ($_SESSION['role'] === 'admin'): ?>
+                    <a class="button" href="/efreinews/public/Admin/index">Admin</a>
                 <?php endif; ?>
-                <a class="button" href="/Mvc/public/Account/logout">Logout</a>
+                <a class="button" href="/efreinews/public/Account/logout">Logout</a>
             <?php else: ?>
-                <a class="button" href="/Mvc/public/Account/login">Login</a>
+                <a class="button" href="/efreinews/public/Account/login">Login</a>
             <?php endif; ?>
         </nav>
     </div>
