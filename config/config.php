26983c61090452d8fc5322d2f89940cdbda65153
Tony <97451137+TonybynMp4@users.noreply.github.com>
2024-10-16 00:37:03 +0200

    feat: remove useless stuff & DEBUG mode

--- config/config.php
+++ config/config.php
@@ -1,12 +1,9 @@
 <?php
 
-// Configuration de la base de données
+define('DEBUG', true);                // Mode debug pour afficher l'option role dans le formulaire d'inscription
+
 define('DB_HOST', 'localhost');       // Hôte de la base de données
 define('DB_NAME', 'efreinews');    // Nom de la base de données
 define('DB_USER', 'pma');            // Utilisateur de la base de données
 define('DB_PASSWORD', null);            // Mot de passe de la base de données
-
-// Autres constantes importantes, par exemple les chemins
-define('CONTROLLER_PATH', __DIR__ . '/../app/controllers/');
-define('VIEW_PATH', __DIR__ . '/../app/views/');
-define('MODEL_PATH', __DIR__ . '/../app/models/');
+?>
