d4d9d65f83be01a28e815d2ce15d5c5b3ae9154b
Tony <97451137+TonybynMp4@users.noreply.github.com>
2024-10-16 00:41:03 +0200

    feat: register page & controller

--- App/controllers/AccountController.php
+++ App/controllers/AccountController.php
@@ -6,6 +6,24 @@ class AccountController extends Controller {
         $this->accountRepository = new AccountRepository();
     }
 
+    public function register() {
+        $args = [];
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $username = $_POST['username'];
+            $password = $_POST['password'];
+            $role = $_POST['role'];
+
+            if ($this->accountRepository->register($username, $password, $role)) {
+                header('Location: /efreinews/public/');
+                exit;
+            } else {
+                $args['error'] = "Erreur lors de l'inscription.";
+            }
+        }
+
+        $this->view('register', $args);
+    }
+
     public function login() {
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $username = $_POST['username'];
---
+++ App/views/register.php
@@ -0,0 +1,27 @@
+<main style="display:flex; flex-direction:column;" x-data="{ error: '<?= $data['error'] ?? '' ?>' }">
+    <template x-if="error">
+        <div x-text="error" style="color: red; margin: 1em auto;"></div>
+    </template>
+    <form action="/efreinews/public/Account/register" method="post">
+        <fieldset style="display:flex; flex-direction:column; width:30%; gap: 1em; margin-inline:auto; padding: 1em; border-radius: 1em;">
+            <legend>
+                Créer un compte
+            </legend>
+            <label for="name">Nom d'utilisateur</label>
+            <input type="text" id="username" name="username" required autofocus placeholder="Nom d'utilisateur">
+            <label for="password">Mot de passe</label>
+            <input type="password" id="password" name="password" required placeholder="Mot de passe">
+            <?php if(DEBUG): ?>
+            <label for="role">[DEBUG] Rôle</label>
+            <select id="role" name="role">
+                <option value="user">Utilisateur</option>
+                <option value="admin">Administrateur</option>
+            </select>
+            <?php endif; ?>
+            <button>Créer un compte</button>
+        </fieldset>
+    </form>
+    <a href="/efreinews/public/Account/login" style="font-style: italic; text-decoration:none; margin: 1em auto;">
+        Déjà inscrit ?
+    </a>
+</main>
